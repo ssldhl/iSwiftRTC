4b6466c5843c5980b9b070c875d587e72e184deb
sushil_dahal <sushil.dahal@techguthi.com>
2016-02-02 12:25:51 +0545

    Initial Commit

---
+++ WebRTC iOS SwiftTests/WebRTC_iOS_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  WebRTC_iOS_SwiftTests.swift
+//  WebRTC iOS SwiftTests
+//
+//  Created by Sushil Dahal on 2/2/16.
+//  Copyright © 2016 Sushil Dahal. All rights reserved.
+//
+
+import XCTest
+@testable import WebRTC_iOS_Swift
+
+class WebRTC_iOS_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
