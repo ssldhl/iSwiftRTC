ee5d5b7a90fa09fea62fb10842adaf2ee18ebdfc
sushil_dahal <sushil.dahal@techguthi.com>
2016-02-04 15:29:11 +0545

    Cleanup AppDelegate

--- WebRTC iOS Swift/AppDelegate.swift
+++ WebRTC iOS Swift/AppDelegate.swift
@@ -29,16 +29,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
-        let iceServerURL: NSURL? = NSURL(string: "'stun:stunserver.org'")
-        let iceServer: RTCICEServer = RTCICEServer(URI: iceServerURL!, username: "", password: "")
-        let iceServers: [RTCICEServer] = [iceServer]
-        
         // Enable SSL globally for WebRTC in our app
         RTCPeerConnectionFactory.initializeSSL()
-        
-        // Create the peer connection using the ICE server list and the current class as the delegate
-        let pcFactory: RTCPeerConnectionFactory = RTCPeerConnectionFactory()
-        let peerConnection: RTCPeerConnection
         
         return true
     }
